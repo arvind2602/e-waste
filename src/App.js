1e982368968e994b1ab4f5e61d854652dca67ea3
arvind2602 <guptaarvind2602@gmail.com>
2023-05-25 01:21:52 +0530

    added

--- src/App.js
+++ src/App.js
@@ -6,8 +6,27 @@ import Contacts from './components/Contacts';
 import Fixit from './components/Fixit';
 import About from './components/About';
 import FixitRepairGuide from './components/FixitRepairGuide';
+import { useEffect } from 'react';
 
 function App() {
+  const googleTranslateElementInit = () => {
+    new window.google.translate.TranslateElement(
+      {
+        pageLanguage: "en",
+        autoDisplay: false
+      },
+      "google_translate_element"
+    );
+  };
+  useEffect(() => {
+    var addScript = document.createElement("script");
+    addScript.setAttribute(
+      "src",
+      "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+    );
+    document.body.appendChild(addScript);
+    window.googleTranslateElementInit = googleTranslateElementInit;
+  }, []);
   return (
     <div className="App">
       <BrowserRouter>
