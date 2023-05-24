ffdec06bf22518c5194e2acc4db7dd73cebf76eb
arvind2602 <guptaarvind2602@gmail.com>
2023-05-24 23:49:08 +0530

    changes

--- src/components/FixitRepairGuide.jsx
+++ src/components/FixitRepairGuide.jsx
@@ -14,7 +14,7 @@ const FixitRepairGuide = () => {
    
     </div>
     <h1 className='h1'>What do you want to repair today</h1>
-    <p className='p'>Repairing your items will help keep them around longer, prolonging their life. ♻️ This will help keep them out of the landfill. 🌍 Making repairs can save you money too, because some items are relatively expensive to replace. 💰 For example, if a small part in your car breaks, and you know how to repair it, it will save you a lot of money compared to buying a whole new car. 🚗💸 </p>
+    <p className='p'>Repairing your items will help keep them around longer, prolonging their life. ♻️ This will help keep them out of the landfill. 🌍 Making repair can save you money too, because some items are relatively expensive to replace. 💰 For example, if a small part in your car breaks, and you know how to repair it, it will save you a lot of money compared to buying a whole new car. 🚗💸 </p>
     <img src={mac} alt='ok' className='im'/>
     <Footer />
     </>
